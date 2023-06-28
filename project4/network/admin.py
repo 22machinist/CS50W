f06ba985916256d2550528ce6b5724ecf444fbb7
22machinist <sharmashutosh22@gmail.com>
2023-06-28 07:17:46 +0530

    Updated admin.py

--- project4/network/admin.py
+++ project4/network/admin.py
@@ -1,3 +1,9 @@
+#Developer : Ashutosh Sharma
 from django.contrib import admin
+from network.models import Post , User , Profile 
 
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(User) 
+admin.site.register(Profile)
