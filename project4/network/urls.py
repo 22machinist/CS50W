d610c06aa0df179b9eefac16a52fd3f72262f93e
22machinist <sharmashutosh22@gmail.com>
2023-06-28 07:06:26 +0530

    Updated urls.py

--- project4/network/urls.py
+++ project4/network/urls.py
@@ -7,5 +7,12 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path('', views.allPost , name="allposts") ,
+    path("u/<username>" , views.profile , name="profile") , 
+    path("following/" , views.following , name="following") , 
+    path("like" , views.like , name="likes") , 
+    path("follow/" , views.follow , name="follow") , 
+    path("edit_post" , views.edit_post , name="edit_post") , 
+    path("addpost/" , views.addpost , name="addpost")
 ]
